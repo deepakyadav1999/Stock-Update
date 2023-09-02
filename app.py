cb4be5de6db15c95487fd000c64568f8181c6221
deepakyadav1999 <50262547+deepakyadav1999@users.noreply.github.com>
2023-09-03 01:12:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,6 +69,6 @@ def download(filename):
         return redirect('/')
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=80)
-    app.run()
+    app.run(host='0.0.0.0', port=80)
+    #app.run()
 
